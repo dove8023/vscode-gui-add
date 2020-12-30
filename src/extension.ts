3670a0524542c8986703910688d2b096c3262f97
hexisen <hexisen@gridsum.com>
2020-12-30 17:34:20 +0800

    feat: 修改插件使用

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import { workspace, commands, ExtensionContext, window, Uri } from 'vscode';
 import { createFile } from './file-helper';
 import { invalidFileNames } from './utils';
-import { basename } from 'path';
 
 export function activate(context: ExtensionContext) {
 
@@ -9,26 +8,31 @@ export function activate(context: ExtensionContext) {
 
 		if (workspace === undefined) {
 			return window.showErrorMessage('Please select a workspace first');
-		} else {
-			return window.showInputBox({
-				placeHolder: "Please enter module name",
-			})
-				.then<any>((input) => {
-					if (input === undefined) { return; }
-
-					if (invalidFileNames.test(input)) {
-						return window.showErrorMessage('Invalid filename');
-					}
-					console.log('resource: ', resource)
-					return createFile({
-						name: input,
-						type: 'module',
-						associatedArray: 'imports',
-						uri: resource,
-						fullName: input.toLowerCase() + `.module.ts`
-					});
-				});
 		}
+
+
+		window.showInputBox({
+			placeHolder: "Please enter component name",
+		})
+			.then<any>((input) => {
+
+
+				let s = window.showQuickPick(["a", "b", "c"])
+				console.log(s)
+
+				if (input === undefined) { return; }
+
+
+				if (invalidFileNames.test(input)) {
+					return window.showErrorMessage('Invalid filename');
+				}
+				console.log('resource: ', resource)
+				return createFile({
+					name: input,
+					uri: resource,
+					fullName: input.toLowerCase() + `.vue`
+				});
+			});
 	});
 
 
